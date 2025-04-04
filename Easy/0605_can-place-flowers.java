3a38ab8d367b50098ad997252e2fe8adca434a50
Karan Shah <karanshah1561998@gmail.com>
2025-04-03 20:05:08 -0400

    605. Can Place Flowers

---
+++ Easy/0605_can-place-flowers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        for (int i = 0; i < len && n > 0; i++) {
+            if (flowerbed[i] == 0) {
+                boolean emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRight = (i == len - 1) || (flowerbed[i + 1] == 0);
+                if (emptyLeft && emptyRight) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        return n == 0;
+    }
+}
