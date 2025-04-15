6215cd3e9c6c2796efa0f94f11775e3485246046
Karan Shah <karanshah1561998@gmail.com>
2025-04-15 14:28:04 -0400

    1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ Medium/1456_maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        int maxCount = 0, count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                count++;
+            }
+            if (i >= k && vowels.contains(s.charAt(i - k))) {
+                count--;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+        return maxCount;
+    }
+}
